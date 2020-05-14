04cb9f7e2e3b4ea8012df196d82fff71828ae287
xieyh <1913603675@qq.com>
2020-05-14 20:29:14 +0800

    删除

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ echo ${JAVA_HOME}
 echo 'Start the program : demo-0.0.1-SNAPSHOT.jar' 
 chmod 777 /home/ldp/app/demo-0.0.1-SNAPSHOT.jar
 echo '-------Starting-------' 
-cd /home/ldp/app/
-nohup ${JAVA_HOME}/bin/java -jar demo-0.0.1-SNAPSHOT.jar server.port=8090 &
+cd /tool/demo/
+nohup ${JAVA_HOME}/bin/java -jar demo-0.0.1-SNAPSHOT.jar &
 echo 'start success'
